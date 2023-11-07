dabc5cbecaa25051847515ebeed67d7ea6125814
rafael marques <rafmando1111@gmail.com>
2023-11-07 14:37:30 +0000

    Add mock product data (3 initial products)

---
+++ src/components/productsData.js
@@ -0,0 +1,38 @@
+export const productsData = [
+  {
+    id: "1",
+    brand: "TREK",
+    name: "Trek Emonda ALR 5 Road Bike",
+    image: "/images/trek-emonda-alr-5-road-bike.jpg",
+    description:
+      "A road bike that’s light, stiff, fast and surprisingly comfortable. Your rides will go longer. You'll barely notice.",
+    price: 1319.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: "2",
+    brand: "PINNACLE",
+    name: "Laterite 1 Road Bike",
+    image: "/images/pinnacle-laterite-1-road-bike.jpg",
+    description:
+      "Easy to maintain internal cable routing keeps cables out of harm’s way and a threaded bottom bracket makes maintenance a breeze. Combined with mounts for full coverage mudguards and rear pannier racks, the Pinnacle Laterite an ideal bike for British conditions.",
+    price: 490.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: "3",
+    brand: "CANNONDALE",
+    name: "CAAD13 Disc Ultegra 2023 Road Bike",
+    image: "/images/cannondale-caad-13-disc-ultegra-2023-road-bike.jpg",
+    description:
+      "For nearly four decades, our CAAD bikes have set the standard for high-performance aluminum frames, and the CAAD13 is the best one yet.",
+    price: 2099.0,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+];
