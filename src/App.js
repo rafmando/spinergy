4c4837f5454607a77f1febd545d5c2a45c4ad72a
rafael marques <rafmando1111@gmail.com>
2023-11-08 14:18:14 +0000

    Add product routing and modify use of router

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import { Hero } from "./components/hero";
 import { Navbar } from "./components/navbar";
 import { Footer } from "./components/footer";
+import { Products } from "./components/products/products";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <Hero />
+      <Routes>
+        <Route exact path="/" element={<Hero />} />
+        <Route exact path="products" element={<Products />} />
+      </Routes>
       <Footer />
-    </>
+    </Router>
   );
 }
 
