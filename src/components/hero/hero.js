838c9a30de16cf24cc6a69e0b10e4f3d98eb07eb
rafael marques <rafmando1111@gmail.com>
2023-11-16 14:02:20 +0000

    Add fixed misc and scroll

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,10 +1,27 @@
 import "../../css/hero.css";
 import { Button } from "../button";
+import { useState } from "react";
 
 export const Hero = () => {
+  const [fixedMisc, setFixedMisc] = useState(true);
+
+  const updateFixedMisc = () => {
+    if (window.scrollY >= 350) {
+      setFixedMisc(false);
+    } else {
+      setFixedMisc(true);
+    }
+    console.log(window.scrollY);
+  };
+
+  window.addEventListener("scroll", updateFixedMisc);
   return (
     <div className="hero-container">
       <img src="images/nick-rickert.jpg" alt="black-bikey"></img>
+      <div className={fixedMisc ? "fixed-misc" : "fixed-misc-yeet"}>
+        Quick and fast delivery
+      </div>
+      <div className="scroll">Scroll down</div>
       <h1>SPINERGY </h1>
       <p>BUY HIGH END BIKES</p>
       <div className="hero-btns">
